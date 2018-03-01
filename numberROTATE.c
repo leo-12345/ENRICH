863b82bedef85405521b8ffb1dd43062a7177f0b
leo-12345 <amarnath.r.dave@gmail.com>
2018-03-01 21:33:34 +0530

    Create numberROTATE.c

---
+++ numberROTATE.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    //number rotate
+    int num,rot,i=0,index=-1,count=0,a[10],j,k,l;
+    for(i=0;i<10;++i)
+    {
+        a[i]=0;
+    }
+    scanf("%d%d",&num,&rot);
+    i=0;
+    while(num>0)
+    {
+        a[i]=num%10;
+        num/=10;
+        i++;
+    }
+    for(j=0,k=i-1;j<i/2;++j,--k)
+    {
+        l=a[k];a[k]=a[j];a[j]=l;
+    }
+    for(j=0;j<i;++j)    
+    {
+        printf("%d",a[(j+rot)%i]);
+    }
+    
+}
