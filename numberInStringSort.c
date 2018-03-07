f89a2665d70f7dcb38009256fe5a20d29c0c06b0
leo-12345 <amarnath.r.dave@gmail.com>
2018-03-07 17:41:38 +0530

    Update numberInStringSort.c

--- numberInStringSort.c
+++ numberInStringSort.c
@@ -3,43 +3,18 @@
 #include <math.h>
 #include <stdlib.h>
 
-//string sort which can handle negative number and preceding zeros.
+//string sort.
 
-int numc(char a[])
-{
-    int i,j=0;
-    for(i=0;i<strlen(a);++i)
-    {
-        if(a[i]>='0'&&a[i]<='9')
-        {
-           j+=a[i]-48;
-           j*=10;
-        }
-    }
-    if(a[0]=='-')
-    {
-        j*=-1;
-    }
-    return j/10;
-}
 int sort(int n,char a[n][10])
 {
-    int i,inta[n],j,k;
+    int i,j,k;
     char s[10];
-    for(i=0;i<n;++i)
-    {
-        inta[i]=numc(a[i]);
-        //printf("%s\n",a[i]);     
-    }
     for(i=0;i<n;++i)
     {
         for(j=i+1;j<n;++j)
         {
-            if(inta[i]>inta[j])
+            if((strcmp(a[i],a[j])>0&&strlen(a[i])==strlen(a[j]))||(strlen(a[i])>strlen(a[j])))
             {
-                k=inta[i];
-                inta[i]=inta[j];
-                inta[j]=k;
                 strcpy(s,a[i]);
                 strcpy(a[i],a[j]);
                 strcpy(a[j],s);
