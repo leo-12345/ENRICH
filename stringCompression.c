be448c777cb8dac69880a5987f046c6b5581e944
leo-12345 <amarnath.r.dave@gmail.com>
2018-03-20 21:19:21 +0530

    Update stringCompression.c

--- stringCompression.c
+++ stringCompression.c
@@ -9,12 +9,16 @@ int compression(char s[100000])
     int i,j;
     for(i=0;i+1<strlen(s);)
     {   for(j=i;j+1<strlen(s)&&s[j]==s[j+1];++j);//check for equality of adjacent characters
-        printf("%c%d",s[i],j-i+1);//print the current character and number of characters that are repeated
+        printf("%c",s[i]);//print the current character and number of characters that are repeated
+        if(j-i+1>1)
+        {
+        printf("%d",j-i+1);
+        }
         i=j+1;
     }
     if(s[strlen(s)-1]!=s[strlen(s)-2])//check for last character
     {
-        printf("%c%d",s[strlen(s)-1],1);
+        printf("%c",s[strlen(s)-1]);
         
     }
     return 0;
