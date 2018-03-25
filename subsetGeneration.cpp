759fc7745e2f53f36a8af062f8dbe53b6aeaa740
leo-12345 <amarnath.r.dave@gmail.com>
2018-03-25 17:34:03 +0530

    Create subsetGeneration.cpp

---
+++ subsetGeneration.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+int x=0;
+//subset generation 
+int showSubsets(int n,int a[10000],int b[10000],int size)
+{
+    int k=0;
+    if(n<0)
+    {
+        return 0;
+    }
+    int j;
+    ++x;
+    j=showSubsets(n-1,a,b,size);
+    for(int i=0;i<size;++i)
+    {
+        k=1;
+        printf("%d ",b[i]);
+    }
+    if(k==1)
+    {
+        printf("\n");
+    }
+    b[size]=a[n];
+    ++size;
+    j=showSubsets(n-1,a,b,size);
+    for(int i=0;i<size;++i)
+    {
+        k=1;
+        printf("%d ",b[i]);
+    }
+    if(k==1)
+    {
+        printf("\n");
+    }
+    
+    return 0;
+}
+//usually there are two possiblities either a number is added in a subset or it is simply skipped so there are 2^n subsets
+int main(int argc, char** argv)
+{
+    int i,j,k,l,m,n;
+    scanf("%d",&n);
+    int a[n],b[n];
+    for(i=0;i<n;++i)
+    {
+        scanf("%d",&a[i]);
+    }
+    showSubsets(n-1,a,b,0);
+    //printf("%d",x); //x denotes number of subsets
+    return 0;
+}
